016946604f25a66eb6c83eecb488e109546a6c41
RedRAINXXXX <49148201+RedRAINXXXX@users.noreply.github.com>
2021-08-19 15:30:01 +0800

    Update img2path.py

--- code/img2path.py
+++ code/img2path.py
@@ -1,20 +1,3 @@
-# Copyright (c) 2020 Huawei Technologies Co., Ltd.
-# Licensed under CC BY-NC-SA 4.0 (Attribution-NonCommercial-ShareAlike 4.0 International) (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     https://creativecommons.org/licenses/by-nc-sa/4.0/legalcode
-#
-# The code is released for academic research use only. For commercial use, please contact Huawei Technologies Co., Ltd.
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
-# This file contains content licensed by https://github.com/xinntao/BasicSR/blob/master/LICENSE/LICENSE
-
-
 import glob
 from natsort import natsort
 import numpy as np
@@ -27,8 +10,8 @@ def fiFindByWildcard(wildcard):
 
 def its():
 
-    lr_dir = 'D:/SRFlow-master/datasets/RESIDE/hazy'
-    hr_dir = 'D:/SRFlow-master/datasets/RESIDE/trans'
+    lr_dir = 'D:/datasets/RESIDE/hazy'
+    hr_dir = 'D:/datasets/RESIDE/gt'
 
     lr_paths = fiFindByWildcard(os.path.join(lr_dir, '*.png'))
 
@@ -44,9 +27,6 @@ def its():
         for i in range(13990):
             f.write(os.path.join(hr_dir,lr_paths[i].split('/')[-1].split('_')[0]+'.png')+'\n')
 
-    # for i in range(2):
-    #     show_from_np(cv2.imread(os.path.join(lr_dir,lr_paths[i])))
-    #     show_from_np(cv2.imread(os.path.join(hr_dir,lr_paths[i].split('/')[-1].split('_')[0]+'.png')))
 
 def ots():
     lr_dir = 'D:\BaiduNetdiskDownload\OTS\hazy'
